df57844a9f6f4edcb5b80eeaf664792b8d6db440
SandalovKY <sandaloff.constantin@yandex.ru>
2020-05-01 23:26:30 +0400

    fin

---
+++ WindowsFormsApp2/MatrixFilters/PrewittFilter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    class PrewittFilter : BorderAllocation
+    {
+        public PrewittFilter()
+        {
+            createPrewittKernel();
+        }
+        protected void createPrewittKernel()
+        {
+            kernel = new float[,]
+                {{-1, 0, 1 },
+                 {-1, 0, 1 },
+                 {-1, 0, 1 }};
+
+        }
+        protected override void changeFilterKernel(int ch)
+        {
+            if (ch == 1)
+            {
+                kernel = new float[,]
+                       {{-1, 0, 1 },
+                        {-1, 0, 1 },
+                        {-1, 0, 1 }};
+            }
+            if (ch == 2)
+            {
+                kernel = new float[,]
+                        {{-1, -1, -1 },
+                         { 0,  0,  0 },
+                         { 1,  1,  1 }};
+            }
+        }
+    };
+}
---
+++ WindowsFormsApp2/MatrixFilters/ScharrFilter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    class ScharrFilter : BorderAllocation
+    {
+        public ScharrFilter()
+        {
+            createScharrKernel();
+        }
+        protected void createScharrKernel()
+        {
+            kernel = new float[,]
+                {{ -3, 0, 3 },
+                 {-10, 0, 10 },
+                 { -3, 0, 3 }};
+
+        }
+        protected override void changeFilterKernel(int ch)
+        {
+            if (ch == 1)
+            {
+                kernel = new float[,]
+                       {{ -3, 0, 3 },
+                        {-10, 0, 10 },
+                        { -3, 0, 3 }};
+            }
+            if (ch == 2)
+            {
+                kernel = new float[,]
+                        {{-3, -10, -3 },
+                         { 0,   0,  0 },
+                         { 3,  10,  3 }};
+            }
+        }
+    };
+}
